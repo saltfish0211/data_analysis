249ff9576d6202ee44a20e36983524d5d1d27d78
BuildTools <unconfigured@null.spigotmc.org>
2023-05-03 15:46:45 +0800

    更新讀取csv檔

--- 統計整合.py
+++ 統計整合.py
@@ -1,3 +1,24 @@
 # coding: utf-8
-#v1.0
+#v1.1  FST 靜止時間計算，計算兩角移動速度來判定
 
+import csv
+from sys import dllhandle
+import pandas as pd
+import numpy as np
+import math
+import glob,os
+
+
+#####################################
+#讀取csv檔並篩選資料col
+#####################################
+def datapath(filepath):
+    path = filepath
+    file = glob.glob(os.path.join(path,"*.csv"))
+    dl = []
+    for f in file:
+        dl.append(pd.read_csv(f,header=2,usecols=["coords","x", "y"]))
+
+
+
+#
